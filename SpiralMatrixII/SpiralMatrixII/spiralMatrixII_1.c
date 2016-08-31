f7a08776210a742df6a73cecce0361f613e6ffac
Elsa Rong <elsarong715@gmail.com>
2016-08-31 23:13:46 +0800

    [CHG]解决输入偶数输出错误问题；简化4个for表达式；修改对输入进行分类讨论的三种情况

--- SpiralMatrixII/SpiralMatrixII/spiralMatrixII_1.c
+++ SpiralMatrixII/SpiralMatrixII/spiralMatrixII_1.c
@@ -10,40 +10,42 @@
 
 void spiralMatrixII_1(int **matrix, int len, int start, int m, int n) {
     
+    /** n=2的情况也可以归类到4个for循环中，没有必要单独区分，只需要单独区分0or1的情况 */
+    if (len == 0) {
+        return;
+    }
+    
+    
     if (len == 1) {
         matrix[m][n] = start;
         return;
     }
     
-    if (len == 2) {
-        matrix[m][n] = start;
-        matrix[m][n++] = start++;
-        matrix[m++][n] = start++;
-        matrix[m][n--] = start++;
-        return;
+    /** 错误原因：前置/后置的++/--是在当前语句执行后再进行的 */
+//    if (len == 2) {
+//        matrix[m][n] = start;
+//        matrix[m][n++] = start++;
+//        matrix[m++][n] = start++;
+//        matrix[m][n] = start++;
+//        return;
+//    }
+    
+    for (int i=n; i<n+len-1; i++) {
+        matrix[m][i] = start++;
+    }
+
+    for (int j=m; j<m+len-1; j++) {
+        matrix[j][n+len-1] = start++;
+    }
+
+    for (int i=n+len-1; i>n; i--) {
+        matrix[m+len-1][i] = start++;
+    }
+
+    for (int j=m+len-1; j>m; j--) {
+        matrix[j][n] = start++;
     }
     
-    for (int i=0; i<len-1; i++) {
-        matrix[m][n+i] = start++;
-    }
-
-    n = n+len-1;
-    for (int j=0; j<len-1; j++) {
-        matrix[m+j][n] = start++;
-    }
-
-    m = m+len-1;
-    for (int i=0; i<len-1; i++) {
-        matrix[m][n-i] = start++;
-    }
-
-    n = n-len+1;
-    for (int j=0; j<len-1; j++) {
-        matrix[m-j][n] = start++;
-    }
-    m = m-len+2;
-    n++;
-    
-    spiralMatrixII_1(matrix, len-2, start, m, n);
+    spiralMatrixII_1(matrix, len-2, start, ++m, ++n);
 
 }
